b6acb7f2dc5dd39f021eb8bd485a3b07a6c0f279
james <james@jamess-MacBook-Pro.local>
2020-06-08 13:53:19 -0700

    hope this works

---
+++ app.py
@@ -0,0 +1,13 @@
+"""Flask app for Cupcakes"""
+from flask import Flask, request
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'SECRETZ'
+
+connect_db(app)
+db.create_all()
